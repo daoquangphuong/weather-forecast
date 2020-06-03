49fdee5e97239098c4425ee7026758275c6c6b49
Dao Quang Phuong <daoquangphuong@gmail.com>
2020-06-03 23:53:26 +0700

    add loading to WeatherDays

--- src/packages/@weather-forecast/components/WeatherDays/container.js
+++ src/packages/@weather-forecast/components/WeatherDays/container.js
@@ -1,12 +1,14 @@
 import { compose, hoc } from '@';
 import { useSelector } from 'react-redux'
-import { selectWeatherDays } from '../../selectors';
+import { selectWeatherDays, selectWeatherDaysLoading } from '../../selectors';
 
 const container = compose(
     hoc(props => {
         const weatherDays = useSelector(selectWeatherDays);
+        const weatherDaysLoading = useSelector(selectWeatherDaysLoading);
 
         return {
+            weatherDaysLoading,
             weatherDays,
             ...props
         };
--- src/packages/@weather-forecast/components/WeatherDays/index.js
+++ src/packages/@weather-forecast/components/WeatherDays/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Spin, Row, Col } from 'antd';
 import WeatherDay from '../WeatherDay';
 import { Root } from './styled';
 import container from './container';
 
 const WeatherDays = props => {
-    const { weatherDays } = props;
+    const { weatherDaysLoading, weatherDays } = props;
     return (
         <Root>
             <Row justify={'center'}>
-                {(weatherDays || []).map(item => <Col key={item.id} xs={24} sm={12} md={6} lg={4}>
-                    <WeatherDay data={item}/>
-                </Col>)}
+                {weatherDaysLoading ?
+                    <Col>
+                        <br/>
+                        <br/>
+                        <Spin size={'large'}/>
+                    </Col>
+                    :
+                    <>
+                        {(weatherDays || []).map(item => <Col key={item.id} xs={24} sm={12} md={6} lg={4}>
+                            <WeatherDay data={item}/>
+                        </Col>)}
+                    </>
+                }
             </Row>
         </Root>
     );
